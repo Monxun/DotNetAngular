7e9f7e26d21fb347785db7be4600f3ea6f495b25
Michael Gibney <michaelgibney1@gmail.com>
2022-10-07 15:00:03 -0700

    updated routing

--- angular/AstreaX/src/app/app-routing.module.ts
+++ angular/AstreaX/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewsComponent } from './news/news.component';
+import { ToolsComponent } from './tools/tools.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'tools', component: ToolsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
