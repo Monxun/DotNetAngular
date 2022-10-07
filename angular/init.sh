1eaa28d3c101609862b5ff9a81dcf3d915d84f92
Michael Gibney <michaelgibney1@gmail.com>
2022-10-07 14:00:00 -0700

    updated project

--- angular/init.sh
+++ angular/init.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+project_name='AstreaX'
+setup_dirs='false'
+create_components='false'
+
 # INSTALL BOOTSTRAP
 npm install bootstrap
 
 # UPDATE styles.css WITH BOOTSTRAP IMPORT
 # Define the filename
-filename='./AstreaX/src/styles.css'
+filename="./$project_name/src/styles.css"
 
 # Type the text that you want to append
-newtext='@import "~bootstrap/dist/css/bootstrap.min.css”;'
+newtext="@import '~bootstrap/dist/css/bootstrap.min.css';"
 
 # Check the new text is empty or not
 if [ "$newtext" != "" ]; then
@@ -17,11 +21,19 @@ if [ "$newtext" != "" ]; then
 fi
 
 # GENERATE NEW MODULES + ROUTING & COMPONENTS
-for i in Home Tasks News Tools; do ng g m "${i}" --routing; done
-for i in Home Tasks News Tools; do ng g c "${i}"; done
+if [ "$create_components" != "false" ]; then
+    cd $project_name
+    for i in Home Tasks News Tools; do ng g m "${i}" --routing; done
+    for i in Home Tasks News Tools; do ng g c "${i}"; done
+    cd ..
+fi
+
 
 # INSTANTIATE TEMPLATES DIR & HTML FILES
-mkdir templates
-cd templates 
-touch app.html home.html tasks.html news.html tools.html
+if [ "$setup_dirs" != "false" ]; then
+    mkdir templates
+    cd templates 
+    touch app.html home.html tasks.html news.html tools.html
+fi
+
 
