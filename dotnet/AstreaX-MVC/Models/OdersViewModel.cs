0b547a88b3eab11f89785b8fd0d279d49b9b0c72
Michael Gibney <michaelgibney1@gmail.com>
2022-10-07 21:13:28 -0700

    Updated MVC

---
+++ dotnet/AstreaX-MVC/Models/OdersViewModel.cs
@@ -0,0 +1,16 @@
+namespace AstreaX_MVC.Models;
+
+public class CustomerOrder
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
+
+public class CustomerOrder
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
