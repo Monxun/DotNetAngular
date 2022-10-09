274e56f89b8679097d27849c4222b16336c6bb7e
Michael Gibney <michaelgibney1@gmail.com>
2022-10-09 16:00:11 -0700

    Updated ViewModel

--- dotnet/AstreaX-MVC/Controllers/CustomerController.cs
+++ dotnet/AstreaX-MVC/Controllers/CustomerController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using AstreaX_MVC.Models;
+using AstreaX_MVC.ViewModels;
 
 namespace AstreaX_MVC.Controllers
 {
@@ -14,7 +16,19 @@ namespace AstreaX_MVC.Controllers
 
         public IActionResult Orders()
         {
-            return View();
+            CustomerViewModel customer = new CustomerViewModel();
+            {
+                customer.CustomerNumber = 4444;
+                customer.FirstName = "Michael";
+                customer.LastName = "Gibney";
+                customer.FullName = "Michael Gibney";
+                customer.Orders = new List<CustomerOrder>();
+                customer.Orders.Add(new CustomerOrder() { OrderId = 1, OrderDate = DateTime.Now, Description = "Overwatch 2 Bundle", Total = 100 });
+                customer.Orders.Add(new CustomerOrder() { OrderId = 2, OrderDate = DateTime.Now, Description = "Ibanez Tim Henson Signature Guitar", Total = 200 });
+                customer.Orders.Add(new CustomerOrder() { OrderId = 3, OrderDate = DateTime.Now, Description = "ML GPU Cluster", Total = 300 });
+            };
+
+            return View(customer);
         }
     }
 }
