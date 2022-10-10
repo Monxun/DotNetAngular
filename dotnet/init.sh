edf43662193a0bd6782de7c2629b94183be154be
Michael Gibney <michaelgibney1@gmail.com>
2022-10-09 19:42:53 -0700

    Updated Scripts

--- dotnet/init.sh
+++ dotnet/init.sh
@@ -4,4 +4,9 @@ mvc_name="AstreaX-MVC"
 
 dotnet new mvc -o $mvc_name
 
+cp -rf ./src/ViewModels/* ./$mvc_name/ViewModels/
+cp -rf ./src/Controllers/* ./$mvc_name/Controllers/
+cp -rf ./src/Views/* ./$mvc_name/Views/
+cp -rf ./src/Models/* ./$mvc_name/Models/
+
 
--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo snap install dotnet-sdk --classic --channel=6.0
-sudo snap alias dotnet-sdk.dotnet dotnet
+if command -v snap >/dev/null 2>&1 ; then
+    echo "Snap found!"
+    echo "version: $(snap -v)"
+    echo "installing dotnet-sdk..."
+    sudo snap install dotnet-sdk --classic --channel=6.0
+    sudo snap alias dotnet-sdk.dotnet dotnet
+else
+    echo "Snap not found! Please install before re-running script..."
+fi
